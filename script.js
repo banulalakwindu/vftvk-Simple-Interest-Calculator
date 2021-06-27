7913ec57ce8e36bca177f5ad65be13f6490dc4c0
Banula Lakwindu <banulalakwindu10@gmail.com>
2021-06-28 02:25:47 +0530

    did the changes to project locally

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // Input values
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    // Compute
+    interest = principal * years * rate / 100;
+    endYear = new Date().getFullYear() + parseInt(years);
+
+    // Display the Result
+    el = document.getElementById("result");
+    if (principal==0 || principal<0) {
+        alert("Enter a positive number!")
+    }
+    else{
+    el.innerHTML = "If you deposit " + principal + "<br/>";
+    el.innerHTML += "at an interest rate of " + rate + "%." + "<br/>";
+    el.innerHTML += "You will recieve an amount of " + interest + ",<br/>";
+    el.innerHTML += "in the year " + endYear + ".<br/>";
+    el.innerHTML = "If you deposit <span class='highlight'>" + principal + "</span><br/>";
+    el.innerHTML += "at an interest rate of <span class='highlight'>" + rate + "%." + "</span><br/>";
+    el.innerHTML += "You will recieve an amount of <span class='highlight'>" + interest + "</span>,<br/>";
+    el.innerHTML += "in the year <span class='highlight'>" + endYear + "</span>.<br/>";
 }
-        
+}
+
+function rateUpdate() {
+    rate = document.getElementById("rate").value;
+    document.getElementById("rate-update").innerHTML = rate + "%";
+}
